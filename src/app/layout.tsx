ac5c7976c428a9f26392de4973649ff25e67502f
PatricioFernandezF <patricio.fernandez.florez@gmail.com>
2024-08-17 22:33:53 +0200

    Barra vertical en layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
+import Header from "@/components/Header";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import Sidebar from "@/components/Sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -21,29 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} dark:bg-gray-900 dark:text-white`}>
         <div className="flex flex-col h-screen">
-          <header className="bg-primary text-primary-foreground py-3 px-4 flex items-center justify-between w-full shadow-lg">
-            <h1 className="text-3xl font-bold text-foreground">CHATIA</h1>
-            <DropdownMenu>
-              <DropdownMenuTrigger>
-                <Avatar className="w-10 h-10 cursor-pointer">
-                  <AvatarImage src="/path-to-avatar-image.jpg" alt="User Avatar" />
-                  <AvatarFallback>U</AvatarFallback>
-                </Avatar>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align="end" className="mt-2">
-                <DropdownMenuItem>
-                  Profile
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                  Settings
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                  Logout
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </header>
-
+          <Header />
           <div className="flex flex-grow overflow-hidden">
             <Sidebar />
             <div className="flex-1 overflow-hidden">
